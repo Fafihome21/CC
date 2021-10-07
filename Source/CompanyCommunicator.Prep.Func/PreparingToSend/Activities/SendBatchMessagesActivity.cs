d57e485673a8d4153b1b010af328636eaa8e59d7
Mohamed Farook Mathersha Maraicar <farook.mathershamaraicar@singtel.com>
2021-10-07 17:19:05 +0000

    Convert to string Updated SendBatchMessagesActivity.cs bug fix

--- Source/CompanyCommunicator.Prep.Func/PreparingToSend/Activities/SendBatchMessagesActivity.cs
+++ Source/CompanyCommunicator.Prep.Func/PreparingToSend/Activities/SendBatchMessagesActivity.cs
@@ -58,7 +58,7 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Prep.Func.PreparingToSend
                 {
                     return new SendQueueMessageContent()
                     {
-                        NotificationId = input.notification.Id,
+                        NotificationId = JsonConvert.SerializeObject(input.notification.Id),
                         IsImportant = input.notification.IsImportant,
                         RecipientData = this.ConvertToRecipientData(recipient),
                     };
